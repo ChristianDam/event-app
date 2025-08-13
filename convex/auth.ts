e1f570e8452053d62c545959c1109c647e0abae0
Christian Dam <christian.dam1995@gmail.com>
2025-08-13 12:38:00 +0200

    Removed unsused

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,8 +1,6 @@
 import { INVALID_PASSWORD } from "./errors.js"
-import GitHub from "@auth/core/providers/github";
 import Google from "@auth/core/providers/google";
 import Resend from "@auth/core/providers/resend";
-import Apple from "@auth/core/providers/apple";
 import { Anonymous } from "@convex-dev/auth/providers/Anonymous";
 import { Password } from "@convex-dev/auth/providers/Password";
 import { ConvexError } from "convex/values";
@@ -13,15 +11,7 @@ import { DataModel } from "./_generated/dataModel.js";
 
 export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
   providers: [
-    GitHub,
     Google,
-    Apple({
-      clientSecret: process.env.AUTH_APPLE_SECRET!,
-      client: {
-        token_endpoint_auth_method: "client_secret_post",
-      },
-      profile: undefined,
-    }),
     Resend({
       from: process.env.AUTH_EMAIL ?? "My App <onboarding@resend.dev>",
     }),
